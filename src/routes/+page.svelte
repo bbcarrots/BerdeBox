5ba18f6a72a629e04585d042aa1ac7ce81fcecb3
ZARAH FLORO <znfloro@up.edu.ph>
2024-04-27 23:15:42 +0800

    feat: install firebase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
+<script>
+	// Import the functions you need from the SDKs you need
+	import { initializeApp } from 'firebase/app';
+	// TODO: Add SDKs for Firebase products that you want to use
+	// https://firebase.google.com/docs/web/setup#available-libraries
+
+	// Your web app's Firebase configuration
+	const firebaseConfig = {
+		apiKey: 'AIzaSyCTTM_Z9FNavPy8Phs5Z_VqOlmneMZcAFM',
+		authDomain: 'cs145-0x0-b0x0-labs-berdebox.firebaseapp.com',
+		projectId: 'cs145-0x0-b0x0-labs-berdebox',
+		storageBucket: 'cs145-0x0-b0x0-labs-berdebox.appspot.com',
+		messagingSenderId: '26502398185',
+		appId: '1:26502398185:web:95512c0a55f89b10f2f078'
+	};
+
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
