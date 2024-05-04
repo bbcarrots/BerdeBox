37f34c1587aa83ee12e4cdae192a75a48ee01289
Allaine Tan <allainericci@gmail.com>
2024-05-04 17:44:11 +0800

    feat: set up initial boxes route loading image from firebase storage

---
+++ src/routes/boxes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+    import BoxPreview from "$lib/components/BoxPreview.svelte";
+    import { getImage } from "$lib/firebase/storage";
+	import { onMount } from "svelte";
+
+    let src: string | null = ""
+    
+    // handle route for "multiple boxes" in the future
+    async function handleRoute(boxNum: number){
+        goto(`/boxes/${boxNum}`);
+    }
+
+    onMount(() => {
+        console.log('Component mounted');
+        getImage('delivery_man.jpeg').then(url => {
+            src = url;
+        });
+    });
+</script>
+
+<BoxPreview {src} on:click={()=>handleRoute(1)}/>
---
+++ src/routes/boxes/[boxID]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    const boxID = Number($page.params.boxID);
+    console.log(boxID)
+</script>
+
+<section>
+    <p>Photo goes here</p>
+</section>
