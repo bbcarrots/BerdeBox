01906589d0baf90588cd5317957e6d6c9a2cfa0e
Allaine Tan <allainericci@gmail.com>
2024-05-04 17:41:21 +0800

    feat: add initial BoxPreview.svelte component

---
+++ src/lib/components/BoxPreview.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let src;
+
+</script>
+
+<button on:click>
+    <div class="w-[500px] h-[250px] bg-gray-100">
+        {#if src == null}
+            No one rang the doorbell
+        {:else}
+            <img src={src} alt="preview"/>
+        {/if}
+    </div>
+</button>
