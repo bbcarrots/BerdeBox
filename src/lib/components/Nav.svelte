f84d1b9a16eb52d7c99fd37873293c0b2477d54d
Allaine Tan <allainericci@gmail.com>
2024-05-04 17:40:59 +0800

    feat: add initial Nav.svelte component

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+
+    $: activeUrl = $page.url.pathname;
+
+    const activeClasses = 'bg-black text-white font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2'
+    const inactiveClasses = 'border border-black font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2'
+</script>
+
+<div>
+    <a href="/boxes">
+        <button type="button" class="{activeUrl === '/boxes' ? activeClasses : inactiveClasses}">
+            Boxes
+        </button>
+    </a>
+    <a href="/modules">
+        <button type="button" class="{activeUrl === '/modules' ? activeClasses : inactiveClasses}">
+            Modules
+        </button>
+    </a>
+</div>
