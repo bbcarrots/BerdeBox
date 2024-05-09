9a6533151b95363000e424c6467c66d23e4e3e0f
Allaine Tan <allainericci@gmail.com>
2024-05-09 17:55:45 +0800

    style: import font and import google fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,25 +2,35 @@
 	import { onMount } from 'svelte';
 	import '../app.css';
 
-	async function detectSWUpdate(){
-		const registration = await navigator.serviceWorker.ready;
+	// async function detectSWUpdate(){
+	// 	const registration = await navigator.serviceWorker.ready;
 
-		registration.addEventListener('updatefound', () =>{
-			const newSW = registration.installing;
-			newSW?.addEventListener('statechange', ()=>{
-				if (newSW.state === 'installed') {
-					if (confirm('New update available! Reload to update?')){
-						newSW.postMessage({type: 'SKIP_WAITING'})
-						window.location.reload();
-					}
-				}
-			})
-		})
-	}
+	// 	registration.addEventListener('updatefound', () =>{
+	// 		const newSW = registration.installing;
+	// 		newSW?.addEventListener('statechange', ()=>{
+	// 			if (newSW.state === 'installed') {
+	// 				if (confirm('New update available! Reload to update?')){
+	// 					newSW.postMessage({type: 'SKIP_WAITING'})
+	// 					window.location.reload();
+	// 				}
+	// 			}
+	// 		})
+	// 	})
+	// }
 
-	onMount(() => {
-		detectSWUpdate();
-	})
+	// onMount(() => {
+	// 	detectSWUpdate();
+	// })
 </script>
 
-<slot />
+<body class="bg-[#EEF2F5]">
+	<slot />
+</body>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Familjen+Grotesk:ital,wght@0,400..700;1,400..700&display=swap');
+
+	:global(body){
+		font-family: "Familjen Grotesk", sans-serif;
+	}
+</style>
