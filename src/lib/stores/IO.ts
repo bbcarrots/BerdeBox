1128db4383c4e8af92a9423b0f8c436dd1b21f3a
allainerain <allainericci@gmail.com>
2024-05-19 19:10:02 +0800

    feat: retrieve multiple photos from firebase storage from date

--- src/lib/stores/IO.ts
+++ src/lib/stores/IO.ts
@@ -1,7 +1,9 @@
-import { writable, type Writable } from "svelte/store";
+import { writable, type Writable } from 'svelte/store';
+import { type log } from '$lib/utils/types';
 
-export const Output: Writable<{[key: string]: boolean | string}> = writable({})
-export const InputOutput: Writable<{[key: string]: boolean | {[key: string]: boolean}}> = writable({})
-export const Input: Writable<{[key: string]: boolean}> = writable({})
+export const Output: Writable<{ [key: string]: boolean | string }> = writable({});
+export const InputOutput: Writable<{ [key: string]: boolean | { [key: string]: boolean } }> =
+	writable({});
+export const Input: Writable<{ [key: string]: boolean }> = writable({});
 
-export const ImageURLs: Writable<string[]> = writable([])
+export const Logs: Writable<log[]> = writable([]);
