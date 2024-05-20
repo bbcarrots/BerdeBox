e95719aab87a6523aba402605d5954ec6255ceec
Allaine Tan <allainericci@gmail.com>
2024-05-20 15:14:37 +0800

    feat: login page UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,18 @@
-<script lan="ts">
-	import {
-		inputOutputRef,
-		inputRef,
-		outputRef,
-		updateButton,
-		updateLED,
-		updateMainLock
-	} from '$lib/firebase/database';
-	import { InputOutput, Output, Input } from '$lib/stores/IO';
-	import { onValue } from '@firebase/database';
-
-	let isOnLed = false;
-	let isOnMainLock = false;
-	let isOnButton = false;
-
-	function handleOutput() {
-		isOnLed = !isOnLed;
-		updateLED(isOnLed);
-	}
-
-	function handleInputOutput() {
-		isOnMainLock = !isOnMainLock;
-		updateMainLock(isOnMainLock);
-	}
-
-	function handleInput() {
-		isOnButton = !isOnButton;
-		updateButton(isOnButton);
-	}
-
-	onValue(outputRef, (snapshot) => {
-		const data = snapshot.val();
-		$Output = data;
-		console.log($Output);
-	});
-
-	onValue(inputOutputRef, (snapshot) => {
-		const data = snapshot.val();
-		$InputOutput = data;
-		console.log($InputOutput);
-	});
-
-	onValue(inputRef, (snapshot) => {
-		const data = snapshot.val();
-		$Input = data;
-		console.log($Input);
-	});
+<script lang="ts">
+	
 </script>
 
-<button on:click={handleOutput}>Button 1!</button>
-<button on:click={handleInputOutput}>Button 2!</button>
-<button on:click={handleInput}>Button 3!</button>
+<section class="grid grid-cols-1 md:grid-cols-2 items-center w-full h-full justify-center">
+	<div class="flex flex-col md:justify-normal gap-8 justify-between md:max-h-[500px] xl:px-[20%] lg:px-[10%] px-[5%] absolute md:relative md:h-auto h-full p-8">
+		<div class="grid gap-4">
+			<h1>Welcome to BerdeBox!</h1>
+			<p>Receive packages safely and securely anywhere, anytime with the smart mailbox you can customize to fit your every need.</p>
+		</div>
+		<button class="flex gap-4 bg-bb-black text-white p-4 rounded-[15px] items-center justify-center">
+			<img class="object-cover w-[20px]" src="/logos/google-logo.svg" alt="google logo"> Continue with Google
+		</button>
+	</div>
+	<div class="bg-red-100 w-full h-screen">
+		<img class="object-cover w-full h-full" src="/mockups/berdebox.png" alt="hero"/>
+	</div>
+</section>
