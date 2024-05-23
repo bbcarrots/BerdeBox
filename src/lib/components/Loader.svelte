c6bbd23f15eb38d66be0d51b9ec5e2596f013186
Allaine Tan <allainericci@gmail.com>
2024-05-23 13:50:30 +0800

    feat: init Loader.svelte component

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<div class="flex -my-4 absolute h-screen w-full bg-white items-center justify-center">
+    <div class="w-[100px]">
+        <img src='/icons/package.gif' alt='animated gif of a box opening' />
+    </div>
+</div>
