73ad24fa477ff334aa5e3b67492dca64073dd4d7
Allaine Tan <allainericci@gmail.com>
2024-05-25 01:46:39 +0800

    feat: add small loader component

---
+++ src/lib/components/SmallLoader.svelte
@@ -0,0 +1,42 @@
+<div class="loader"></div>
+
+
+<style>
+    .loader {
+        font-size: 3px;
+        width: 1em;
+        height: 1em;
+        border-radius: 50%;
+        position: relative;
+        text-indent: -9999em;
+        animation: mulShdSpin 1s infinite ease;
+        transform: translateZ(0);
+    }
+    @keyframes mulShdSpin {
+    0%,
+    100% {
+        box-shadow: 0em -2.6em 0em 0em #FFFFFF, 1.8em -1.8em 0 0em #D9D9D9, 2.5em 0em 0 0em #4C4C4C, 1.75em 1.75em 0 0em #4C4C4C, 0em 2.5em 0 0em #4C4C4C, -1.8em 1.8em 0 0em #4C4C4C, -2.6em 0em 0 0em #8F8F8F, -1.8em -1.8em 0 0em #BCBCBC;
+    }
+    12.5% {
+        box-shadow: 0em -2.6em 0em 0em #BCBCBC, 1.8em -1.8em 0 0em #FFFFFF, 2.5em 0em 0 0em #D9D9D9, 1.75em 1.75em 0 0em #4C4C4C, 0em 2.5em 0 0em #4C4C4C, -1.8em 1.8em 0 0em #4C4C4C, -2.6em 0em 0 0em #4C4C4C, -1.8em -1.8em 0 0em #8F8F8F;
+    }
+    25% {
+        box-shadow: 0em -2.6em 0em 0em #8F8F8F, 1.8em -1.8em 0 0em #BCBCBC, 2.5em 0em 0 0em #FFFFFF, 1.75em 1.75em 0 0em #D9D9D9, 0em 2.5em 0 0em #4C4C4C, -1.8em 1.8em 0 0em #4C4C4C, -2.6em 0em 0 0em #4C4C4C, -1.8em -1.8em 0 0em #4C4C4C;
+    }
+    37.5% {
+        box-shadow: 0em -2.6em 0em 0em #4C4C4C, 1.8em -1.8em 0 0em #8F8F8F, 2.5em 0em 0 0em #BCBCBC, 1.75em 1.75em 0 0em #FFFFFF, 0em 2.5em 0 0em #D9D9D9, -1.8em 1.8em 0 0em #4C4C4C, -2.6em 0em 0 0em #4C4C4C, -1.8em -1.8em 0 0em #4C4C4C;
+    }
+    50% {
+        box-shadow: 0em -2.6em 0em 0em #4C4C4C, 1.8em -1.8em 0 0em #4C4C4C, 2.5em 0em 0 0em #8F8F8F, 1.75em 1.75em 0 0em #BCBCBC, 0em 2.5em 0 0em #FFFFFF, -1.8em 1.8em 0 0em #D9D9D9, -2.6em 0em 0 0em #4C4C4C, -1.8em -1.8em 0 0em #4C4C4C;
+    }
+    62.5% {
+        box-shadow: 0em -2.6em 0em 0em #4C4C4C, 1.8em -1.8em 0 0em #4C4C4C, 2.5em 0em 0 0em #4C4C4C, 1.75em 1.75em 0 0em #8F8F8F, 0em 2.5em 0 0em #BCBCBC, -1.8em 1.8em 0 0em #FFFFFF, -2.6em 0em 0 0em #D9D9D9, -1.8em -1.8em 0 0em #4C4C4C;
+    }
+    75% {
+        box-shadow: 0em -2.6em 0em 0em #4C4C4C, 1.8em -1.8em 0 0em #4C4C4C, 2.5em 0em 0 0em #4C4C4C, 1.75em 1.75em 0 0em #4C4C4C, 0em 2.5em 0 0em #8F8F8F, -1.8em 1.8em 0 0em #BCBCBC, -2.6em 0em 0 0em #FFFFFF, -1.8em -1.8em 0 0em #D9D9D9;
+    }
+    87.5% {
+        box-shadow: 0em -2.6em 0em 0em #4C4C4C, 1.8em -1.8em 0 0em #4C4C4C, 2.5em 0em 0 0em #4C4C4C, 1.75em 1.75em 0 0em #4C4C4C, 0em 2.5em 0 0em #4C4C4C, -1.8em 1.8em 0 0em #8F8F8F, -2.6em 0em 0 0em #BCBCBC, -1.8em -1.8em 0 0em #FFFFFF;
+    }
+}
+</style>
