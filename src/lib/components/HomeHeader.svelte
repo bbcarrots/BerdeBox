279c73f115ae723616083b3bbb05c45053224f77
allainerain <allainericci@gmail.com>
2024-05-25 22:13:20 +0800

    feat: add loader for profile image

--- src/lib/components/HomeHeader.svelte
+++ src/lib/components/HomeHeader.svelte
@@ -1,5 +1,8 @@
 <script>
 	import { UserStore } from '$lib/stores/User';
+	import SmallLoader from './SmallLoader.svelte';
+
+	let loading = true;
 </script>
 
 <div class="flex gap-4 pt-4 my-4 justify-between items-center">
@@ -7,10 +10,22 @@
 		<h3>BerdeBox</h3>
 		<p class="">Track your deliveries</p>
 	</div>
-	<img
-		class="rounded-full w-[60px] h-[60px]"
-		src={$UserStore.profilePhoto}
-		alt="user profile"
-		referrerpolicy="no-referrer"
-	/>
+
+	<div class={loading ? 'hidden' : 'block'}>
+		<img
+			on:load={() => {
+				loading = false;
+			}}
+			class="rounded-full w-[60px] h-[60px]"
+			src={$UserStore.profilePhoto}
+			alt="user profile"
+			referrerpolicy="no-referrer"
+		/>
+	</div>
+
+	{#if loading}
+		<div class="bg-white flex rounded-full w-[60px] h-[60px] items-center justify-center">
+			<SmallLoader />
+		</div>
+	{/if}
 </div>
