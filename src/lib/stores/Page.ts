f53b1bf844990752727f7b16cb625b6009a0f12d
allainerain <allainericci@gmail.com>
2024-05-27 16:17:28 +0800

    feat: retrieve updated photos from firebase and add loading

--- src/lib/stores/Page.ts
+++ src/lib/stores/Page.ts
@@ -1,5 +1,6 @@
 import { writable, type Writable } from 'svelte/store';
 
+export let loginLoading: Writable<boolean> = writable(false);
 export let loading: Writable<boolean> = writable(false);
 export let retrievingBoxes: Writable<boolean> = writable(false);
 export let loadingProfilePhoto: Writable<boolean> = writable(true);
