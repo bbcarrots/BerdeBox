3331a37d2e736fd22d7c75d4cb654373f3c82a4c
allainerain <allainericci@gmail.com>
2024-05-28 20:48:32 +0800

    fix: reset uth to firebaseui

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -1,7 +1,13 @@
 import firebase from 'firebase/compat/app';
 import 'firebaseui/dist/firebaseui.css';
 import { firebaseApp } from './config';
-import { getAuth, getRedirectResult, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import {
+	getAuth,
+	getRedirectResult,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut
+} from 'firebase/auth';
 import { goto } from '$app/navigation';
 
 export const auth = getAuth(firebaseApp);
@@ -13,60 +19,60 @@ const provider = new GoogleAuthProvider();
 auth.languageCode = 'it';
 
 // //redirect on auth
-export const login = () => {
+// export const login = () => {
 
-	signInWithPopup(auth, provider)
-	.then((result) => {
-		// This gives you a Google Access Token. You can use it to access Google APIs.
-		let credential = null; // Initialize credential as null
+// 	signInWithPopup(auth, provider)
+// 	.then((result) => {
+// 		// This gives you a Google Access Token. You can use it to access Google APIs.
+// 		let credential = null; // Initialize credential as null
 
-		if (result) {
-			credential = GoogleAuthProvider.credentialFromResult(result);
-			const user = result.user;
-			console.log(user);
-		}
-		const token = credential?.accessToken;
+// 		if (result) {
+// 			credential = GoogleAuthProvider.credentialFromResult(result);
+// 			const user = result.user;
+// 			console.log(user);
+// 		}
+// 		const token = credential?.accessToken;
 
-		// The signed-in user info.
-		const user = result?.user;
-		// IdP data available using getAdditionalUserInfo(result)
-		// ...
-	})
-	.catch((error) => {
-		// Handle Errors here.
-		const errorCode = error.code;
-		const errorMessage = error.message;
-		// The email of the user's account used.
-		const email = error.customData.email;
-		// The AuthCredential type that was used.
-		const credential = GoogleAuthProvider.credentialFromError(error);
-		// ...
-	});
+// 		// The signed-in user info.
+// 		const user = result?.user;
+// 		// IdP data available using getAdditionalUserInfo(result)
+// 		// ...
+// 	})
+// 	.catch((error) => {
+// 		// Handle Errors here.
+// 		const errorCode = error.code;
+// 		const errorMessage = error.message;
+// 		// The email of the user's account used.
+// 		const email = error.customData.email;
+// 		// The AuthCredential type that was used.
+// 		const credential = GoogleAuthProvider.credentialFromError(error);
+// 		// ...
+// 	});
+// }
+
+//needs to be in a function since firebaseui needs to be dynamically imported
+export async function getUIConfig() {
+	return {
+		signInSuccessUrl: '/',
+		signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+		tosUrl: '/',
+		privacyPolicyUrl: function () {
+			window.location.assign('/');
+		},
+		signInFlow: 'popup'
+	};
 }
 
-// //needs to be in a function since firebaseui needs to be dynamically imported
-// export async function getUIConfig() {
-// 	return {
-// 		signInSuccessUrl: '/',
-// 		signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-// 		tosUrl: '/',
-// 		privacyPolicyUrl: function () {
-// 			window.location.assign('/');
-// 		},
-// 		signInFlow: "popup"
-// 	};
-// }
+//initialize the firebase auth ui
+export async function initializeFirebaseUI(): Promise<void> {
+	if (typeof window !== 'undefined') {
+		const firebaseui = await import('firebaseui');
+		const uiConfig = await getUIConfig();
 
-// //initialize the firebase auth ui
-// export async function initializeFirebaseUI(): Promise<void> {
-// 	if (typeof window !== 'undefined') {
-// 		const firebaseui = await import('firebaseui');
-// 		const uiConfig = await getUIConfig();
-
-// 		var ui = new firebaseui.auth.AuthUI(auth);
-// 		ui.start('#firebaseui-auth-container', uiConfig);
-// 	}
-// }
+		var ui = new firebaseui.auth.AuthUI(auth);
+		ui.start('#firebaseui-auth-container', uiConfig);
+	}
+}
 
 export function handleSignOut() {
 	signOut(auth)
@@ -81,29 +87,29 @@ export function handleSignOut() {
 		});
 }
 
-// export function initApp() {
-// 	console.log('app init');
-// 	firebase.auth().onAuthStateChanged(function (user) {
-// 		console.log('auth change');
-// 		if (user) {
-// 			// User is signed in.
-// 			console.log('user', user);
-// 			var displayName = user.displayName;
-// 			var email = user.email;
-// 			var emailVerified = user.emailVerified;
-// 			var photoURL = user.photoURL;
-// 			var uid = user.uid;
-// 			var phoneNumber = user.phoneNumber;
-// 			var providerData = user.providerData;
+export function initApp() {
+	console.log('app init');
+	firebase.auth().onAuthStateChanged(function (user) {
+		console.log('auth change');
+		if (user) {
+			// User is signed in.
+			console.log('user', user);
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var uid = user.uid;
+			var phoneNumber = user.phoneNumber;
+			var providerData = user.providerData;
 
-// 			user.getIdToken().then(
-// 				function (accessToken) {
-// 					console.log(accessToken);
-// 				},
-// 				function (error) {
-// 					console.log(error);
-// 				}
-// 			);
-// 		}
-// 	});
-// }
+			user.getIdToken().then(
+				function (accessToken) {
+					console.log(accessToken);
+				},
+				function (error) {
+					console.log(error);
+				}
+			);
+		}
+	});
+}
