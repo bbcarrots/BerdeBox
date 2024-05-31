26cbc6a0e79e69215566f804238b0194dfe4e3b5
Allaine Tan <allainericci@gmail.com>
2024-05-31 20:33:25 +0800

    chore: use product photo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,15 +14,15 @@
 		class="flex flex-col md:justify-normal gap-8 justify-between md:max-h-[500px] xl:px-[20%] lg:px-[10%] px-[5%] absolute md:relative md:h-auto h-full p-8"
 	>
 		<div class="grid gap-4">
-			<h1>Welcome to BerdeBox!</h1>
-			<p>
+			<h1 class="text-black max-md:text-center">Welcome to BerdeBox!</h1>
+			<p class="max-md:text-center ">
 				Receive packages safely and securely anywhere, anytime with the smart mailbox you can
 				customize to fit your every need.
 			</p>
 		</div>
 		<!-- <div id="firebaseui-auth-container"> -->
 		<button
-			class="flex gap-4 bg-bb-black text-white p-4 rounded-[15px] items-center justify-center"
+			class="flex gap-4 bg-black text-white p-4 rounded-[15px] items-center justify-center"
 			on:click={login}
 		>
 			<img class="object-cover w-[20px]" src="/logos/google-logo.svg" alt="google logo" /> Continue with
@@ -31,6 +31,6 @@
 		<!-- </div> -->
 	</div>
 	<div class="w-full h-svh">
-		<img class="object-cover w-full h-full" src="/mockups/berdebox.png" alt="hero" />
+		<img class="object-cover w-full h-full" src="/mockups/1.png" alt="hero" />
 	</div>
 </section>
